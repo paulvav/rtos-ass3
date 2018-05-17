5726d3ddbe3587b452f0946ed175591be120c7cc
paulvav <paulvav@gmail.com>
2018-05-17 18:57:53 +1000

    first commit

---
+++ prog1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+
+
+
+//Declare struct used for procceses
+struct proccess
+{
+        char id;
+        int arriveTime;
+        int bustTime;
+};
+
+struct proccess proccesses[
+
+int main()
+{
+
+
+
+}
