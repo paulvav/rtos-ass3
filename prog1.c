aa6fc365cc2c8071c31dc0c581cca828d68413a1
VMUSER <paulvav@gmail.com>
2018-05-20 00:07:01 +1000

    working through fifo issue

--- prog1.c
+++ prog1.c
@@ -28,7 +28,9 @@ struct proccess proccesses[7] = {
 
 
 void *A();
-pthread_t tid1;
+void *B();
+
+pthread_t tid1,tid2;
 pthread_attr_t attr;
 
 char * fifo = "fifo";
@@ -37,36 +39,43 @@ char * fifo = "fifo";
 
 int main()
 {
+        if (mkfifo(fifo, 0666) < 0)
+                printf("Error creating named pipe.\n");
+        else
+                printf("pipe made");
+
+
+
 	
 	pthread_attr_init(&attr);
 	pthread_create(&tid1,&attr,A,NULL);
+	pthread_create(&tid2,&attr,B,NULL);
 	pthread_join(tid1,NULL);
+//	pthread_join(tid2,NULL);
+//	pthread_join(tid1,NULL);
 }
+
 
 void *A()
 {
-	if (mkfifo(fifo, 0666) < 0)
-		printf("Error creating named pipe.\n");
-	else 
-		printf("pipe made");
-	printf("DEBUG: We have arrived at thread A");
 	int initialise = 0;
 	int clock;
 	int totalWait=0;
 	int averageWait;
 	int fd;	
-
+        
+        printf("Thread A FLAG\n\n");
 	//Open FIFO for write only proccess
 	//fd = open(fifo, O_WRONLY);
 
-	for (clock=0;clock<60;clock++)
+	for (clock=0;clock<10;clock++)
 	{
 		int smallestBustTime = 15;
 		int proc;
 		for(proc=0;proc<7;proc++)
 		{
 		
-		 	printf("Proc: %c, ArriveTime: %d, Clock: %d, bustTime: %d, smallestBustTime: %d, waitTime: %d\n", proccesses[proc].id,proccesses[proc].arriveTime,clock,proccesses[proc].bustTime, smallestBustTime, proccesses[proc].waitTime);	
+		 	//intf("Proc: %c, ArriveTime: %d, Clock: %d, bustTime: %d, smallestBustTime: %d, waitTime: %d\n", proccesses[proc].id,proccesses[proc].arriveTime,clock,proccesses[proc].bustTime, smallestBustTime, proccesses[proc].waitTime);	
 			if(proccesses[proc].arriveTime <= clock)
 			{
 				if(proccesses[proc].bustTime > 0)
@@ -95,20 +104,33 @@ void *A()
 		totalWait = totalWait + proccesses[proc].waitTime;		
 	}
 	averageWait = totalWait / sizeof(proccesses);
-        fd=open(fifo, O_WRONLY);	
-	write(fd,(void *)&averageWait,100);
-	close(fd);
+        if (fd=open(fifo, O_WRONLY) == -1)
+		printf("PIPE OPEN ERROR");	
 
+	write(fd,"balls",strlen("balls"));
+	 
+	close(fd);
+	return;
 }
 
 void *B()
 {
 	char string[10];
 	int fd;
-	fd=open(fifo,ORDONLY);
-	read(fd,string,80);
-	printf("%s",string
-
+	printf("THread B baby\n\n");
+	//fd = open(fifo,O_RDONLY);
+	
+	while(1)
+	{
+	        fd = open(fifo,O_RDONLY);
+		read(fd,&string,5);
+		printf("string: %s\n\n", string);
+		close(fd);
+		if(string == "bls")
+			break;
+		//printf("%s",string);
+	}
+return;
 
 
 
